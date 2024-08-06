9264ddf50499f8c5d15ce5ac1fbffa8bce5950d4
shatunidze <ekatyashatunova@yandex.ru>
2024-08-06 16:36:32 +0300

    описание типов данных и методы работы с ними

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export interface IProductItem {
+    id: string,
+    image: string,
+    category: string,
+    title: string,
+    description: string,
+    price: number | null
+}
+
+export interface IProductList {
+    items: IProductItem[],
+    total: number
+}
+
+export interface IUser {
+    payment: string,
+    email: string,
+    addres: string,
+    phone: number
+}
+
+export interface IBasketData {
+    addProduct(items: IProductItem): void;
+    deleteProduct(items: IProductItem): void;
+    getAllProducts(items: IProductItem[]): void;
+    checkBasketValidation(id: string): boolean;
+    getTotalPrice(): number;
+    getProductsCounter(): number;
+    clearBasket(): void;
+}
+
+export interface IUserData {
+    getUserData(): IUser; 
+    setUserData(userData: IUser): void;
+    checkValidationOrder(data: Record<keyof TUserOrder, string>): boolean;
+}
+
+export type TUserOrder = Pick<IUser, 'payment' | 'addres'>;
+export type TUserContacts = Pick<IUser, 'email' | 'phone'>;
+export type TBasketOrder = Pick<IProductItem, 'title' | 'price' | 'id'>
