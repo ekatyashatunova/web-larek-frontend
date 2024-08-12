e5183b0c183fd07561c8c590ee050c84a5292758
shatunidze <ekatyashatunova@yandex.ru>
2024-08-12 23:58:13 +0300

    index.ts 12.08

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 import './scss/styles.scss';
+import {EventEmitter} from '../src/components/base/events';
+import {UserData} from './components/base/UserData';
+import {IEvents} from './components/base/events';
 
+const events: IEvents = new EventEmitter();
+const userData = new UserData(events);
