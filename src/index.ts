7df330a6bc864c6505a0683504ca844a9ab64343
shatunidze <ekatyashatunova@yandex.ru>
2024-08-13 23:16:41 +0300

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import './scss/styles.scss';
-import {EventEmitter} from '../src/components/base/events';
-import {UserData} from './components/base/UserData';
-import {IEvents} from './components/base/events';
+import {EventEmitter, IEvents} from '../src/components/base/events';
+import {UserData} from './components/UserData';
+import {BasketData} from './components/BasketData';
+import {Catalog} from './components/Catalog';
 
 const events: IEvents = new EventEmitter();
 const userData = new UserData(events);
+const basketData = new BasketData(events);
+const catalog = new Catalog();
