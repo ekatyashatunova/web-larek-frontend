756a507f239afc70e571948e5b1e1e360c8f0b3f
shatunidze <ekatyashatunova@yandex.ru>
2024-08-15 00:28:03 +0300

    модель данных catalog

--- src/components/Catalog.ts
+++ src/components/Catalog.ts
@@ -1,5 +1,25 @@
-import {ICatalog} from '../types/index';
+import {ICatalog, IProductItem, IProductList} from '../types/index';
+import {IEvents} from '../components/base/events';
 
-export class Catalog implements ICatalog {
-    protected 
-}
+
+
+/*export class Catalog implements ICatalog {
+    protected _items: IProductList;
+    protected _total: number;
+    protected events: IEvents;
+
+    constructor(events: IEvents) {
+        this.events = events;
+    }
+    
+    setProduct(products: IProductList): void {
+  this._items = products
+    }
+
+    getProduct() {
+        return this._items
+    }
+
+
+}*/
+
