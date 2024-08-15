705220eb8735793be17ad22c199cd11a077dc239
shatunidze <ekatyashatunova@yandex.ru>
2024-08-15 23:40:52 +0300

    catalog

--- src/components/Catalog.ts
+++ src/components/Catalog.ts
@@ -1,25 +1,24 @@
-import {ICatalog, IProductItem, IProductList} from '../types/index';
+import {ICatalog, IProductItem} from '../types/index';
 import {IEvents} from '../components/base/events';
 
 
 
-/*export class Catalog implements ICatalog {
-    protected _items: IProductList;
-    protected _total: number;
+export class Catalog implements ICatalog {
+    protected _items: IProductItem[];
     protected events: IEvents;
 
     constructor(events: IEvents) {
         this.events = events;
     }
     
-    setProduct(products: IProductList): void {
-  this._items = products
+    setProduct(products: IProductItem[]): void {
+        this._items = products
     }
 
-    getProduct() {
-        return this._items
+    getProduct(): IProductItem[] {
+        return this._items 
     }
 
 
-}*/
+}
 
