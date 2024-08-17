a3b83534939383ac7882a104acbf3c2ff66ff555
shatunidze <ekatyashatunova@yandex.ru>
2024-08-17 13:30:41 +0300

    класс представления productcard

--- src/components/ProductCard.ts
+++ src/components/ProductCard.ts
@@ -1,5 +1,6 @@
-import { createElement } from "../utils/utils";
+import { cloneTemplate} from "../utils/utils";
 import { IEvents } from "./base/events";
+import {IProductItem} from '../types'
 
 export class ProductCard {
     protected element: HTMLElement;
@@ -8,11 +9,54 @@ export class ProductCard {
     protected cardTitle: HTMLElement;
     protected cardImage: HTMLImageElement;
     protected cardPrice: HTMLElement;
-    protected description: HTMLElement;
+    protected cardDescription: HTMLElement;
+    protected cardId: string;
+    protected cardButton: HTMLButtonElement;
 
     constructor(template: HTMLTemplateElement, events: IEvents) {
         this.events = events;
-        this.element = createElement(template)
+        this.element = cloneTemplate(template);
+
+        this.cardCategory = this.element.querySelector('.card__category');
+        this.cardTitle = this.element.querySelector('.card__title');
+        this.cardImage = this.element.querySelector('.card__image');
+        this.cardPrice = this.element.querySelector('.card__price');
+        this.cardDescription = this.element.querySelector('.card__text');
+        this.cardButton = this.element.querySelector('.gallery__item');
+
+        this.cardButton.addEventListener('click', () => {
+            this.events.emit('gallery__item:open', {card: this})
+        })
     }
 
-}
+    setData(productData: IProductItem) {
+        if (this.cardCategory) {
+        this.cardCategory.textContent = productData.category;
+    }
+        this.cardTitle.textContent = productData.title;
+
+        if(this.cardDescription){
+            this.cardDescription.textContent = productData.description;
+        }
+
+        if(this.cardPrice === null) {
+            this.cardPrice.textContent = 'Бесценно' 
+        } else {
+            this.cardPrice.textContent = productData.toString() + 'синапсов'
+        }
+
+        if(this.cardImage){
+            this.cardImage.src = productData.image;
+      }
+     }
+
+     render() {
+        return this.element 
+    }
+}     
+    
+
+   
+
+    
+
