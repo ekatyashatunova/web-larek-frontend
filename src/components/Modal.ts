b17c1cd08e563356420b3bdf187fab364a893468
shatunidze <ekatyashatunova@yandex.ru>
2024-09-16 01:09:00 +0300

    modal.ts 16.09

--- src/components/Modal.ts
+++ src/components/Modal.ts
@@ -1,21 +1,49 @@
 import { Component } from "./base/Component";
 import { IEvents } from "./base/events";
-interface IModal {
-    content: HTMLElement;
-}
 
-export class Modal extends Component<IModal> {
+/*interface IModal {
+  modal: HTMLElement;
+}*/
+
+export class Modal<T> extends Component<T> {
     protected _closeButton: HTMLButtonElement;
-    protected _content: HTMLElement;
+    protected _modal: HTMLElement;
+    protected events: IEvents
 
-    constructor(container:HTMLElement, protected events: IEvents) {
+    constructor(container:HTMLElement, events: IEvents) {
         super(container);
+        this.events = events;
 
-        this._closeButton = this.container.querySelector('.modal__close');
-        this._content = this.container.querySelector('.modal__content');
+       this._closeButton = document.querySelector('.modal__close');
+        this._modal = document.querySelector('.modal__content');
 
-        this._closeButton.addEventListener('click', this.close.bind(this))
-
-        close()
+       
+        this._closeButton.addEventListener('click', this.close.bind(this));
+        /*this.container.addEventListener('click', this.close.bind(this));*/
+        
+        
+        this._modal.addEventListener('click', (event) => event.stopPropagation());
     }
-}
+
+    set modal(value: HTMLElement) {
+      this._modal.replaceChildren(value);
+    }
+        
+        open() {
+          this.container.classList.add('modal_active');
+          this.events.emit('modal:close');
+        }
+        
+        close() {
+            this.container.classList.remove('modal_active');
+            this._modal = null;
+            this.events.emit('modal:close');
+        }
+
+        render(data: T): HTMLElement {
+          super.render(data);
+          this.open();
+          return this.container;
+        }
+    }
+
