f0d2803f9889f9994abc0ba8c7acebec59e1731b
shatunidze <ekatyashatunova@yandex.ru>
2024-09-16 01:08:41 +0300

    form.ts 16.09

---
+++ src/components/Form.ts
@@ -0,0 +1,58 @@
+import { Component } from "./base/Component";
+import { IEvents } from "./base/events";
+
+interface IForm {
+    valid: boolean;
+    inputValues: Record<string, string>;
+    errors: Record<string, string>;
+}
+
+export class Form extends Component<IForm> {
+    protected inputs: NodeListOf<HTMLInputElement>;
+    protected submitButton: HTMLButtonElement;
+    protected formName: string;
+    protected _form: HTMLFormElement;
+    protected _errors: Record<string, HTMLElement>;
+
+    constructor(container: HTMLElement, protected events: IEvents) {
+        super(container);
+
+        this.inputs = this.container.querySelectorAll<HTMLInputElement>('.form__input');
+        this._form = this.container.querySelector('.form')
+        this.formName = this._form.getAttribute('name');
+        this.submitButton = this.container.querySelector('button[type=submit]');
+
+        this._errors = {};
+        this.inputs.forEach((input) => {
+            this._errors[input.name] = this._form.querySelector('.form__errors');
+        })
+    
+        this._form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+            this.events.emit(`${this.formName}:submit`, this.getInputValues());
+        })
+
+        this._form.addEventListener('input', (evt: Event) => {
+            const target = evt.target as HTMLInputElement;
+            const field = target.name;
+            const value = target.value;
+            this.events.emit(`${this.formName}:input`, {field, value})
+        });
+    }
+
+    protected getInputValues() {
+        const valuesObject: Record<string, string> = {};
+        this.inputs.forEach((element) => {
+            valuesObject[element.name] = element.name;
+        })
+        return valuesObject
+    }
+
+    set inputValues(data: Record<string, string>) {
+        this.inputs.forEach((element) => {
+            element.value = data[element.name]
+        })
+        
+
+}
+}
