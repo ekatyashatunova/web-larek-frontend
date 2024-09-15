92a48d132b274d7e2c03bd64d893896f818f6e2c
shatunidze <ekatyashatunova@yandex.ru>
2024-09-16 01:09:11 +0300

    page.ts 16.09

--- src/components/Page.ts
+++ src/components/Page.ts
@@ -3,13 +3,15 @@ import {IEvents } from "./base/events";
 
 export interface IPage {
     cardsCatalog: HTMLElement[];
-    counterBasket: HTMLElement
+    counterBasket: HTMLElement;
+    /*locked: boolean*/
 }
 
 export class Page extends Component<IPage> {
     protected _cardsCatalog: HTMLElement[];
     protected _counterBasket: HTMLElement;
     protected buttonBasket: HTMLButtonElement;
+    /*protected _wrapper: HTMLElement;*/
 
     constructor(protected container: HTMLElement, protected events: IEvents) {
         super(container);
@@ -17,6 +19,7 @@ export class Page extends Component<IPage> {
 
         this.buttonBasket = document.querySelector('.header__basket');
         this._counterBasket = this.container.querySelector('.basket__price');
+        /*this._wrapper = this.container.querySelector('.page__wrapper');*/
 
         
        this.buttonBasket.addEventListener('click', () => {
@@ -28,5 +31,13 @@ export class Page extends Component<IPage> {
     set cardsCatalog(items: HTMLElement[]) {
         this.container.replaceChildren(...items);
     }
+
+    /*set locked(value: boolean) {
+        if (value) {
+            this._wrapper.classList.add('page__wrapper_locked');
+        } else {
+            this._wrapper.classList.remove('page__wrapper_locked');
+        }
+    }*/
 }
 
