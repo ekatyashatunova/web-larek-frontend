297104adddff5d3874f9c9d34b3b5e646c099ee6
shatunidze <ekatyashatunova@yandex.ru>
2024-09-16 01:10:06 +0300

    successpay.ts 16.09

---
+++ src/components/SuccessPay.ts
@@ -0,0 +1,29 @@
+import { Component } from "./base/Component"
+interface ISuccessPay {
+    total: number
+}
+
+interface ISuccessActions {
+    onClick: () => void;
+}
+
+//Класс представления успешной покупки
+export class SuccessPay extends Component<ISuccessPay> {
+    protected buttonSuccess: HTMLButtonElement;
+    protected _total: HTMLButtonElement
+
+    constructor(container: HTMLElement, actions: ISuccessActions) {
+        super(container);
+
+        this.buttonSuccess = this.container.querySelector('order-success__close');
+        this._total = this.container.querySelector('.order-success__description');
+
+        if (actions?.onClick) {
+            this.buttonSuccess.addEventListener('click', actions.onClick);
+        }
+    }
+
+    set total(total:number) {
+        this._total.textContent = 'Cписано '+ total.toString() + ' синапсов';
+    }
+}
