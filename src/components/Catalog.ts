8c73ee24e6f90e47a49eade0160f297ee6ddf2c6
shatunidze <ekatyashatunova@yandex.ru>
2024-09-15 13:44:22 +0300

    catalog.ts 15.09

--- src/components/Catalog.ts
+++ src/components/Catalog.ts
@@ -5,6 +5,7 @@ import {IEvents} from '../components/base/events';
 //Модель данных каталога товаров
 export class Catalog implements ICatalog {
     protected products: IProductItem[];
+    protected _preview: string | null;
     protected events: IEvents;
 
     constructor(events: IEvents) {
@@ -12,11 +13,22 @@ export class Catalog implements ICatalog {
     }
     
     setProduct(items: IProductItem[]) {
-        this.products =  items
+        this.products =  items;
+        this.events.emit('products:changed');
     }
 
-    getProduct(): IProductItem[] {
+    getProducts(): IProductItem[] {
         return this.products
+    }
+
+    getProduct(productId: string) {
+        return this.products.find((data) => data.id === productId)
+    }
+
+   
+
+    get preview() {
+        return this._preview
     }
 }
 
