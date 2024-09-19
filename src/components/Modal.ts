7ef35fdae084c288b02448c7da877263db23c563
shatunidze <ekatyashatunova@yandex.ru>
2024-09-19 23:06:41 +0300

    modal.ts 19.09

--- src/components/Modal.ts
+++ src/components/Modal.ts
@@ -18,8 +18,8 @@ export class Modal extends Component<IModal> {
         this._content = ensureElement<HTMLElement>('.modal__content', container);
         
         this._closeButton.addEventListener('click', this.close.bind(this));
-        this.container.addEventListener('click',this.close.bind(this)) ;
         this._content.addEventListener('click', (event) => event.stopPropagation());
+        this.handleEscUp = this.handleEscUp.bind(this);
     }
 
     set content(value: HTMLElement) {
@@ -28,16 +28,24 @@ export class Modal extends Component<IModal> {
         
         open() {
           this.container.classList.add('modal_active');
+          document.addEventListener("keyup", this.handleEscUp);
           this.events.emit('modal:open');
         }
         
         close() {
             this.container.classList.remove('modal_active');
+            document.removeEventListener("keyup", this.handleEscUp);
             /*this.content.remove();*/
             this.content = null;
             this.events.emit('modal:close');
         }
 
+        handleEscUp (evt: KeyboardEvent) {
+          if (evt.key === "Escape") {
+            this.close();
+          }
+        };
+    
         render(data: IModal): HTMLElement {
           super.render(data);
           this.open();
