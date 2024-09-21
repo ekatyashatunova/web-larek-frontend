f7339af318b67740f1aa455d26f579aa37466ad0
shatunidze <ekatyashatunova@yandex.ru>
2024-09-21 12:55:29 +0300

    contactsform.ts 21.09

--- src/components/ContactsForm.ts
+++ src/components/ContactsForm.ts
@@ -1,11 +1,21 @@
+import { Form } from './Form';
 import { IEvents } from './base/events';
 import {TUserContacts} from '../types';
-import { Component } from './base/Component';
+
 
 //Класс представления форма с контактами (телефон, эл.почта)
-export class ContactsForm extends Component<TUserContacts> {
-    protected _email: HTMLElement;
-    protected _phone: HTMLElement;
-    protected events: IEvents
+export class ContactsForm extends Form<TUserContacts> {
+   constructor(container: HTMLElement, events: IEvents) {
+    super(container, events);
+   }
+
+  /* set email(value: string) {
+    document.querySelector('[name="email"]').value = value;
+}
+
+set phone(value: string) {
+    (this.container.elements.namedItem('phone') as HTMLInputElement).value = value;
+}
+   */ 
 
 }
