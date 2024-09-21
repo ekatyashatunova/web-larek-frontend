7d4c81be329aa411cebc755ef19378274df25dd4
shatunidze <ekatyashatunova@yandex.ru>
2024-09-21 12:55:43 +0300

    paymentform.ts 21.09

--- src/components/PaymentForm.ts
+++ src/components/PaymentForm.ts
@@ -1,15 +1,15 @@
-import { Component} from "./base/Component";
+import { Form } from './Form';
 import { TUserOrder } from "../types";
 import { IEvents } from "./base/events";
 
 
 //Класс представления формы с адресом и способом оплаты
-export class PaymentForm extends Component<TUserOrder> {
+export class PaymentForm<T> extends Form<TUserOrder> {
     protected payment: HTMLElement;
     protected address: HTMLElement;
 
     constructor(container: HTMLElement, protected events: IEvents) {
-        super(container);
+        super(container, events);
     }
 
 }
