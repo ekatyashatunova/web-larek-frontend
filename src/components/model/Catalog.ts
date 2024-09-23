7fd064c39234523f21d0a7f0915188d8d363d859
shatunidze <ekatyashatunova@yandex.ru>
2024-09-23 22:59:55 +0300

    catalog.ts

---
+++ src/components/model/Catalog.ts
@@ -0,0 +1,44 @@
+import {ICatalog, IProductItem, IProductList} from '../../types';
+import {IEvents} from '../base/events';
+
+
+//Модель данных каталога товаров
+export class Catalog implements ICatalog {
+    protected products: IProductItem[];
+    protected _preview: string | null;
+    protected events: IEvents;
+
+    constructor(events: IEvents) {
+        this.events = events;
+    }
+    
+    setProduct(items: IProductItem[]) {
+        this.products =  items;
+        this.events.emit('products:changed');
+    }
+
+    getProducts(): IProductItem[] {
+        return this.products
+    }
+
+    getProduct(cardId: string) {
+        return this.products.find((data) => data.id === cardId)
+    }
+
+    /*set preview(cardId: string | null) {
+        if (!cardId) {
+            this._preview = null;
+            return;
+        }
+        const selectedCard = this.getProduct(cardId);
+        if (selectedCard) {
+            this._preview = cardId;
+            this.events.emit('card:selected')
+        }
+    }
+
+    get preview() {
+        return this._preview
+    }*/
+}
+
