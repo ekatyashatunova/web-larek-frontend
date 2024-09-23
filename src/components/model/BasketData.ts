99b3e20c072c1ea4cb83b7da997ce872db1aa9b8
shatunidze <ekatyashatunova@yandex.ru>
2024-09-23 22:59:45 +0300

    basketdata.ts

---
+++ src/components/model/BasketData.ts
@@ -0,0 +1,47 @@
+import {IBasketData, IProductItem, IProductList} from '../../types/index'
+import { IEvents } from '../base/events';
+
+//Модель данных товаров с корзиной
+export class BasketData implements IBasketData {
+    protected _products: IProductItem[];
+    protected events: IEvents;
+
+    constructor(events: IEvents) {
+        this.events = events;
+    }
+
+    set products(products:IProductItem[]) {
+        this._products = products;
+        /*this.events.emit('basket:changed')*/
+    }   
+
+    getAllProducts(): IProductItem[] {
+        return this._products;
+    }
+
+    addProduct(product: IProductItem) {
+        this._products = [...this._products, product]
+        /*this.events.emit('basket:changed') */  
+    }
+
+    deleteProduct(product: IProductItem) {
+        this._products = this._products.filter((data) => data.id === product.id);
+        /*this.events.emit('basket:changed') */
+    }
+
+    checkBasket(id: string): boolean {
+       return this._products.some((product) => product.id === id) 
+    }
+
+    getTotalPrice(): number {
+        return this._products.reduce((sum, item) => sum + item.price, 0);
+    
+    }
+    getProductsCounter() {
+        return this._products.length
+        
+    }
+    clearBasket(): void {
+        this._products = []
+    }
+}
