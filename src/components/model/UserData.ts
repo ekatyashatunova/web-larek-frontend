d78d7093137e30e8ff11e539de2f08cba49c9422
shatunidze <ekatyashatunova@yandex.ru>
2024-09-23 23:00:03 +0300

    userdata.ts

---
+++ src/components/model/UserData.ts
@@ -0,0 +1,39 @@
+import {IUser, IUserData, TUserOrder, TUserContacts} from '../../types/index';
+import {IEvents} from '../base/events';
+
+//Модель данных покупателя
+export class UserData implements IUserData {
+    protected payment: string;
+    protected email: string;
+    protected address: string;
+    protected phone: string ;
+    protected events: IEvents 
+
+    constructor(events: IEvents) {
+        this.events = events;
+    }
+
+    getUserOrder(): IUser {
+        return {payment: this.payment, email: this.email, address: this.address, phone: this.phone}
+    }
+
+    setUserOrder(userData: IUser) {
+        this.payment = userData.payment;
+        this.email = userData.email;
+        this.address = userData.address;
+        this.phone = userData.phone;
+        this.events.emit('user:changed');
+    }
+
+    /*checkValidationOrder(data: Record<keyof TUserOrder, string>): boolean {
+        const isValidAddress = data.address.includes('');
+        if (!isValidAddress) {
+            this.events.emit('inValidAddress');
+            return false
+        }
+            return true
+        }*/
+    }
+
+
+
