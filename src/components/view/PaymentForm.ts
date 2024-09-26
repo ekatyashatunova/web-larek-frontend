1819ed6f9e78e95eff2a4c6e819b72fdccf9117c
shatunidze <ekatyashatunova@yandex.ru>
2024-09-27 02:04:24 +0300

    paymentform.ts 27.09

--- src/components/view/PaymentForm.ts
+++ src/components/view/PaymentForm.ts
@@ -4,12 +4,34 @@ import { IEvents } from "../base/events";
 
 
 //Класс представления формы с адресом и способом оплаты
-export class PaymentForm<T> extends Form<TUserOrder> {
-    protected payment: HTMLElement;
-    protected address: HTMLElement;
+export class PaymentForm extends Form<TUserOrder> {
+    protected paymentCard: HTMLButtonElement;
+    protected paymentCash: HTMLButtonElement;
+    protected address: HTMLInputElement;
 
-    constructor(container: HTMLElement, protected events: IEvents) {
+    constructor(container: HTMLFormElement, protected events: IEvents) {
         super(container, events);
+
+        this.paymentCash = this.container.cash as HTMLButtonElement; /*querySelector('order__buttons[name=cash]');*/
+        this.paymentCard = this.container.card as HTMLButtonElement; /*querySelector('order__buttons[name=card]');*/ 
+        this.address = this.container.querySelector('input[name=address]');
+
+        this.paymentCard.addEventListener('click', () => {
+            this.events.emit('payment:select', {payment: 'card'})
+        })
+
+        this.paymentCash.addEventListener('click', () => {
+            this.events.emit('payment:select', {payment: 'cash'})
+        })
+    }
+    
+   set _payment(value: string) {
+    this.toggleClass(this.paymentCard, 'button_alt-active', value === 'card');
+    this.toggleClass(this.paymentCash, 'button_alt-active', value === 'cash');
+   }
+
+    set _address(value: string) {
+        (this.container.elements.namedItem('address') as HTMLInputElement).value = value;
     }
 
 }
