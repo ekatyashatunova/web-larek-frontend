d380f45e03c28f4d78669cddeaedac31d6f754a2
shatunidze <ekatyashatunova@yandex.ru>
2024-09-27 21:57:52 +0300

    contactsform.ts 27.09

--- src/components/view/ContactsForm.ts
+++ src/components/view/ContactsForm.ts
@@ -7,22 +7,14 @@ import {TUserContacts} from '../../types';
 export class ContactsForm extends Form<TUserContacts> {
     protected phone: HTMLInputElement;
     protected email: HTMLInputElement;
-    /*protected buttonSubmit: HTMLButtonElement;*/
     
     constructor(container: HTMLFormElement, events: IEvents) {
         super(container, events);
 
         this.phone = this.container.querySelector('input[name=phone]');
         this.email = this.container.querySelector('input[name=email]');
-        /*this.buttonSubmit = this.container.querySelector('button') as HTMLButtonElement;
-
-        this.buttonSubmit.addEventListener('click', () => {
-            this.events.emit('contactsForm:open');*/
-    };
+}
         
-
-
-   
    set _phone(value: string) {
     (this.container.elements.namedItem('phone') as HTMLInputElement).value = value;
 }
