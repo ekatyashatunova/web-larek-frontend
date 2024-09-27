628416ef4918396c629dcbd4537a1ae109d7bf04
shatunidze <ekatyashatunova@yandex.ru>
2024-09-27 23:01:29 +0300

    successpay.ts 27.09

--- src/components/view/SuccessPay.ts
+++ src/components/view/SuccessPay.ts
@@ -1,3 +1,4 @@
+import { ensureElement } from "../../utils/utils";
 import { Component } from "../base/Component";
 import { IEvents } from "../base/events";
 interface ISuccessPay {
@@ -6,17 +7,16 @@ interface ISuccessPay {
 
 //Класс представления успешной покупки
 export class SuccessPay extends Component<ISuccessPay> {
-    protected buttonSuccess: HTMLButtonElement;
-    protected _total: HTMLButtonElement
+    protected buttonSuccessClose: HTMLButtonElement;
+    protected _total: HTMLElement;
 
     constructor(container: HTMLElement, protected events: IEvents) {
         super(container);
 
-        this.buttonSuccess = this.container.querySelector('order-success__close');
-        this._total = this.container.querySelector('.order-success__description');
+        this.buttonSuccessClose = ensureElement<HTMLButtonElement>('.order-success__close', container);
+        this._total = ensureElement<HTMLElement>('.order-success__description', container);
 
-       
-            this.buttonSuccess.addEventListener('click', () => {
+           this.buttonSuccessClose.addEventListener('click', () => {
                 this.events.emit('order:success')
             });
     }
