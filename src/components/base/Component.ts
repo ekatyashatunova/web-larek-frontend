6612b1334009204bc878a748e29568beb05cfa4b
shatunidze <ekatyashatunova@yandex.ru>
2024-09-28 11:45:47 +0300

    component 28.09

--- src/components/base/Component.ts
+++ src/components/base/Component.ts
@@ -1,20 +1,18 @@
 export abstract class Component<T> {
-    constructor(protected readonly container: HTMLElement) {
+	constructor(protected readonly container: HTMLElement) {}
 
-    }
+	protected setText(element: HTMLElement, value: unknown) {
+		if (element) {
+			element.textContent = String(value);
+		}
+	}
 
-    protected setText(element: HTMLElement, value: unknown) {
-        if (element) {
-            element.textContent = String(value);
-        }
-    }
+	toggleClass(element: HTMLElement, className: string, force?: boolean) {
+		element.classList.toggle(className, force);
+	}
 
-    toggleClass(element: HTMLElement, className: string, force?: boolean) {
-        element.classList.toggle(className, force);
-    }
-
-    render(data?: Partial<T>): HTMLElement {
-        Object.assign(this as object, data ?? {});
-        return this.container
-    }
+	render(data?: Partial<T>): HTMLElement {
+		Object.assign(this as object, data ?? {});
+		return this.container;
+	}
 }
