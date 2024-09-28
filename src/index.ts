a7bd8199cdbc0f1b69eed884a3797d947f02c913
shatunidze <ekatyashatunova@yandex.ru>
2024-09-28 16:24:25 +0300

    index.ts 28.09

--- src/index.ts
+++ src/index.ts
@@ -208,6 +208,7 @@ events.on('formErrors:change', (errors: Partial<TUserContacts>) => {
 	contactsForm.errors = Object.values({ phone, email })
 		.filter((i) => !!i)
 		.join('; ');
+        contactsForm.clearContactsForm()
 });
 
 //Клик по кнопке "Оплатить" в форме с данными покупателя
