9014fca7da6d9dc64f0ec9d19038cd36fd5e7628
shatunidze <ekatyashatunova@yandex.ru>
2024-09-28 11:47:30 +0300

    successpay.ts 28.09

--- src/components/view/SuccessPay.ts
+++ src/components/view/SuccessPay.ts
@@ -1,27 +1,33 @@
-import { ensureElement } from "../../utils/utils";
-import { Component } from "../base/Component";
-import { IEvents } from "../base/events";
+import { ensureElement } from '../../utils/utils';
+import { Component } from '../base/Component';
+import { IEvents } from '../base/events';
 interface ISuccessPay {
-    total: number
+	total: number;
 }
 
 //Класс представления успешной покупки
 export class SuccessPay extends Component<ISuccessPay> {
-    protected buttonSuccessClose: HTMLButtonElement;
-    protected _total: HTMLElement;
+	protected buttonSuccessClose: HTMLButtonElement;
+	protected _total: HTMLElement;
 
-    constructor(container: HTMLElement, protected events: IEvents) {
-        super(container);
+	constructor(container: HTMLElement, protected events: IEvents) {
+		super(container);
 
-        this.buttonSuccessClose = ensureElement<HTMLButtonElement>('.order-success__close', container);
-        this._total = ensureElement<HTMLElement>('.order-success__description', container);
+		this.buttonSuccessClose = ensureElement<HTMLButtonElement>(
+			'.order-success__close',
+			container
+		);
+		this._total = ensureElement<HTMLElement>(
+			'.order-success__description',
+			container
+		);
 
-           this.buttonSuccessClose.addEventListener('click', () => {
-                this.events.emit('order:success')
-            });
-    }
+		this.buttonSuccessClose.addEventListener('click', () => {
+			this.events.emit('order:success');
+		});
+	}
 
-    set total(total:number) {
-        this._total.textContent = 'Cписано '+ total.toString() + ' синапсов';
-    }
+	set total(total: number) {
+		this._total.textContent = 'Cписано ' + total.toString() + ' синапсов';
+	}
 }
