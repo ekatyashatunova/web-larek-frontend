1645a50b9c2ae791ff80f76123e8961eb9c69dea
shatunidze <ekatyashatunova@yandex.ru>
2024-09-28 11:45:38 +0300

    appapi 28.09

--- src/components/base/AppApi.ts
+++ src/components/base/AppApi.ts
@@ -1,17 +1,21 @@
-import {IApi, IOrderData, IResponseOrder, IProductList} from '../../types';
+import { IApi, IOrderData, IResponseOrder, IProductList } from '../../types';
 
 export class AppApi {
-    private _baseApi: IApi;
+	private _baseApi: IApi;
 
-    constructor(baseApi: IApi) {
-        this._baseApi = baseApi;
-    }
+	constructor(baseApi: IApi) {
+		this._baseApi = baseApi;
+	}
 
-    getProducts(): Promise<IProductList> {
-        return this._baseApi.get<IProductList>(`/product`).then((products: IProductList) => products)
-    }
+	getProducts(): Promise<IProductList> {
+		return this._baseApi
+			.get<IProductList>(`/product`)
+			.then((products: IProductList) => products);
+	}
 
-   postOrder(data: IOrderData): Promise<IResponseOrder> {
-    return this._baseApi.post<IResponseOrder>(`/order`, data, `POST`).then((res:IResponseOrder ) => res)
-   }
+	postOrder(data: IOrderData): Promise<IResponseOrder> {
+		return this._baseApi
+			.post<IResponseOrder>(`/order`, data, `POST`)
+			.then((res: IResponseOrder) => res);
+	}
 }
