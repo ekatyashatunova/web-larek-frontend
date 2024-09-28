9a3c5a2c620ff5ed17a42a0f3590ff4ff39ebd55
shatunidze <ekatyashatunova@yandex.ru>
2024-09-28 11:46:34 +0300

    basket.ts 28.09

--- src/components/view/Basket.ts
+++ src/components/view/Basket.ts
@@ -1,49 +1,50 @@
-import { Component } from "../base/Component";
-import { IEvents } from "../base/events";
-import { createElement } from "../../utils/utils"; 
+import { Component } from '../base/Component';
+import { IEvents } from '../base/events';
+import { createElement } from '../../utils/utils';
 
 interface IBasket {
-    products: HTMLElement[],
-    total: number,
+	products: HTMLElement[];
+	total: number;
 }
 
 //Класс представления корзины с товарами
 export class Basket extends Component<IBasket> {
-    protected basketList: HTMLElement;
-    protected basketPrice: HTMLElement;
-    protected basketIndex: HTMLElement;
-    protected orderButton: HTMLButtonElement;
-    
-    constructor(protected container: HTMLElement, protected events: IEvents) {
-        super(container);
+	protected basketList: HTMLElement;
+	protected basketPrice: HTMLElement;
+	protected basketIndex: HTMLElement;
+	protected orderButton: HTMLButtonElement;
 
-        this.basketList =  this.container.querySelector('.basket__list');
-        this.basketPrice = this.container.querySelector('.basket__price');
-        this.orderButton = this.container.querySelector('.basket__button');
+	constructor(protected container: HTMLElement, protected events: IEvents) {
+		super(container);
 
-        this.orderButton.addEventListener('click', () => {
-            this.events.emit('form:open')    
-        })
-}
+		this.basketList = this.container.querySelector('.basket__list');
+		this.basketPrice = this.container.querySelector('.basket__price');
+		this.orderButton = this.container.querySelector('.basket__button');
 
-set products(products: HTMLElement[]) {
-    if (products.length) {
-        this.basketList.replaceChildren(...products);
-        this.orderButton.disabled = false;
-    } else {
-        this.basketList.replaceChildren(createElement<HTMLParagraphElement>('p', {
-            textContent: 'Корзина пуста'
-        }));
-        this.orderButton.disabled = true;
-    }
-}
+		this.orderButton.addEventListener('click', () => {
+			this.events.emit('form:open');
+		});
+	}
 
-set productIndex(productIndex: number) {
-    this.basketIndex.textContent = productIndex.toString();
-}
+	set products(products: HTMLElement[]) {
+		if (products.length) {
+			this.basketList.replaceChildren(...products);
+			this.orderButton.disabled = false;
+		} else {
+			this.basketList.replaceChildren(
+				createElement<HTMLParagraphElement>('p', {
+					textContent: 'Корзина пуста',
+				})
+			);
+			this.orderButton.disabled = true;
+		}
+	}
 
-set total(total: number) {
-    this.basketPrice.textContent = total.toString() + ' синапсов';
-}
+	set productIndex(productIndex: number) {
+		this.basketIndex.textContent = productIndex.toString();
+	}
 
+	set total(total: number) {
+		this.basketPrice.textContent = total.toString() + ' синапсов';
+	}
 }
