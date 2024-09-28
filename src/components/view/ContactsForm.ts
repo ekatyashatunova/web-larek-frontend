ae1687ff1dbf3325f4d951b1da82891977c3d965
shatunidze <ekatyashatunova@yandex.ru>
2024-09-28 11:46:44 +0300

    contactsform 28.09

--- src/components/view/ContactsForm.ts
+++ src/components/view/ContactsForm.ts
@@ -1,26 +1,26 @@
 import { Form } from './Form';
 import { IEvents } from '../base/events';
-import {TUserContacts} from '../../types';
-
+import { TUserContacts } from '../../types';
 
 //Класс представления форма с контактами (телефон, эл.почта)
 export class ContactsForm extends Form<TUserContacts> {
-    protected phone: HTMLInputElement;
-    protected email: HTMLInputElement;
-    
-    constructor(container: HTMLFormElement, events: IEvents) {
-        super(container, events);
+	protected phone: HTMLInputElement;
+	protected email: HTMLInputElement;
 
-        this.phone = this.container.querySelector('input[name=phone]');
-        this.email = this.container.querySelector('input[name=email]');
-}
-        
-   set _phone(value: string) {
-    (this.container.elements.namedItem('phone') as HTMLInputElement).value = value;
-}
+	constructor(container: HTMLFormElement, events: IEvents) {
+		super(container, events);
 
-set _email(value: string) {
-    (this.container.elements.namedItem('email') as HTMLInputElement).value = value;
-}
+		this.phone = this.container.querySelector('input[name=phone]');
+		this.email = this.container.querySelector('input[name=email]');
+	}
 
+	set _phone(value: string) {
+		(this.container.elements.namedItem('phone') as HTMLInputElement).value =
+			value;
+	}
+
+	set _email(value: string) {
+		(this.container.elements.namedItem('email') as HTMLInputElement).value =
+			value;
+	}
 }
