6921913f29b77140ae19a61cc3321d8d43b66e84
shatunidze <ekatyashatunova@yandex.ru>
2024-09-28 11:47:19 +0300

    paymentform.ts 28.09

--- src/components/view/PaymentForm.ts
+++ src/components/view/PaymentForm.ts
@@ -1,36 +1,36 @@
 import { Form } from './Form';
-import { TUserOrder } from "../../types";
-import { IEvents } from "../base/events";
+import { TUserOrder } from '../../types';
+import { IEvents } from '../base/events';
 
 //Класс представления формы с адресом и способом оплаты
 export class PaymentForm extends Form<TUserOrder> {
-    protected paymentCard: HTMLButtonElement;
-    protected paymentCash: HTMLButtonElement;
-    protected addressInput: HTMLInputElement;
+	protected paymentCard: HTMLButtonElement;
+	protected paymentCash: HTMLButtonElement;
+	protected addressInput: HTMLInputElement;
 
-    constructor(container: HTMLFormElement, protected events: IEvents) {
-        super(container, events);
+	constructor(container: HTMLFormElement, protected events: IEvents) {
+		super(container, events);
 
-        this.paymentCash = this.container.cash as HTMLButtonElement; 
-        this.paymentCard = this.container.card as HTMLButtonElement; 
-        this.addressInput = this.container.querySelector('input[name=address]');
+		this.paymentCash = this.container.cash as HTMLButtonElement;
+		this.paymentCard = this.container.card as HTMLButtonElement;
+		this.addressInput = this.container.querySelector('input[name=address]');
 
-        this.paymentCard.addEventListener('click', () => {
-            this.onInputChange('payment', 'card')
-        })
+		this.paymentCard.addEventListener('click', () => {
+			this.onInputChange('payment', 'card');
+		});
 
-        this.paymentCash.addEventListener('click', () => {
-            this.onInputChange('payment', 'cash')
-        })
-    }
-    
-   set payment(value: string) {
-    this.toggleClass(this.paymentCard, 'button_alt-active', value === 'card');
-    this.toggleClass(this.paymentCash, 'button_alt-active', value === 'cash');
-   }
+		this.paymentCash.addEventListener('click', () => {
+			this.onInputChange('payment', 'cash');
+		});
+	}
 
-    set address(value: string) {
-        (this.container.elements.namedItem('address') as HTMLInputElement).value = value;
-    }
+	set payment(value: string) {
+		this.toggleClass(this.paymentCard, 'button_alt-active', value === 'card');
+		this.toggleClass(this.paymentCash, 'button_alt-active', value === 'cash');
+	}
 
+	set address(value: string) {
+		(this.container.elements.namedItem('address') as HTMLInputElement).value =
+			value;
+	}
 }
