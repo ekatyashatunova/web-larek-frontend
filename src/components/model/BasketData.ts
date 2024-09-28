0cbb53d25a0a823c8746edc9e4d57e7766296ea6
shatunidze <ekatyashatunova@yandex.ru>
2024-09-28 11:45:55 +0300

    basketdata 28.09

--- src/components/model/BasketData.ts
+++ src/components/model/BasketData.ts
@@ -1,51 +1,51 @@
-import {IBasketData, IProductItem, IProductList} from '../../types/index'
+import { IBasketData, IProductItem, IProductList } from '../../types/index';
 import { IEvents } from '../base/events';
 
 //Модель данных товаров с корзиной
 export class BasketData implements IBasketData {
-    protected _products: IProductItem[] = [];
-    protected events: IEvents;
+	protected _products: IProductItem[] = [];
+	protected events: IEvents;
 
-    constructor(events: IEvents) {
-        this.events = events;
-    }
+	constructor(events: IEvents) {
+		this.events = events;
+	}
 
-    set products(products:IProductItem[]) {
-        this._products = products;
-    }   
+	set products(products: IProductItem[]) {
+		this._products = products;
+	}
 
-    getAllProducts(): IProductItem[] {
-        return this._products;
-    }
+	getAllProducts(): IProductItem[] {
+		return this._products;
+	}
 
-    addProduct(product: IProductItem) {
-        this._products = [...this._products, product]
-        this.events.emit('basket:add'); 
-    }
+	addProduct(product: IProductItem) {
+		this._products = [...this._products, product];
+		this.events.emit('basket:add');
+	}
 
-    deleteProduct(product: IProductItem) {
-        this._products = this._products.filter((data) => data.id !== product.id);
-        this.events.emit('basket:delete');
-        console.log(this._products)
-    }
+	deleteProduct(product: IProductItem) {
+		this._products = this._products.filter((data) => data.id !== product.id);
+		this.events.emit('basket:delete');
+		console.log(this._products);
+	}
 
-    checkBasket(id: string): boolean {
-       return this._products.some((product) => product.id === id) 
-    }
+	checkBasket(id: string): boolean {
+		return this._products.some((product) => product.id === id);
+	}
 
-    getTotalPrice(): number {
-        return this._products.reduce((sum, product) => sum + product.price, 0);
-    }
+	getTotalPrice(): number {
+		return this._products.reduce((sum, product) => sum + product.price, 0);
+	}
 
-    getProductsCounter() {
-        return this._products.length  
-    }
+	getProductsCounter() {
+		return this._products.length;
+	}
 
-    getProductIds(): string[] {
-        return this._products.map(item => item.id); 
-      }
-      
-    clearBasket() {
-        this._products = []
-    }
+	getProductIds(): string[] {
+		return this._products.map((item) => item.id);
+	}
+
+	clearBasket() {
+		this._products = [];
+	}
 }
