423b01921c8bf879f6d9122dcaaef0bb04be0fea
shatunidze <ekatyashatunova@yandex.ru>
2024-09-28 11:46:05 +0300

    catalog.ts 28.09

--- src/components/model/Catalog.ts
+++ src/components/model/Catalog.ts
@@ -1,27 +1,25 @@
-import {ICatalog, IProductItem, IProductList} from '../../types';
-import {IEvents} from '../base/events';
-
+import { ICatalog, IProductItem, IProductList } from '../../types';
+import { IEvents } from '../base/events';
 
 //Модель данных каталога товаров
 export class Catalog implements ICatalog {
-    protected products: IProductItem[];
-    protected events: IEvents;
+	protected products: IProductItem[];
+	protected events: IEvents;
 
-    constructor(events: IEvents) {
-        this.events = events;
-    }
-    
-    setProduct(items: IProductItem[]) {
-        this.products =  items;
-        this.events.emit('productsCard:loaded');
-    }
+	constructor(events: IEvents) {
+		this.events = events;
+	}
 
-    getProducts(): IProductItem[] {
-        return this.products
-    }
+	setProduct(items: IProductItem[]) {
+		this.products = items;
+		this.events.emit('productsCard:loaded');
+	}
 
-    getProduct(cardId: string) {
-        return this.products.find((data) => data.id === cardId)
-    }
+	getProducts(): IProductItem[] {
+		return this.products;
+	}
+
+	getProduct(cardId: string) {
+		return this.products.find((data) => data.id === cardId);
+	}
 }
-
