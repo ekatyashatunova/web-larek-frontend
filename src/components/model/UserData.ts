8877454cddf607a739fcf6fae6cd71f501a8cce2
shatunidze <ekatyashatunova@yandex.ru>
2024-09-28 11:46:25 +0300

    userdata.ts 28.09

--- src/components/model/UserData.ts
+++ src/components/model/UserData.ts
@@ -1,92 +1,80 @@
-import {IUser, IUserData, TUserOrder, TUserContacts, FormErrors} from '../../types/index';
-import {IEvents} from '../base/events';
+import {
+	IUser,
+	IUserData,
+	TUserOrder,
+	TUserContacts,
+	FormErrors,
+} from '../../types/index';
+import { IEvents } from '../base/events';
 
 //Модель данных покупателя
-export class UserData /*implements IUserData*/ {
-    protected payment: string;
-    protected email: string;
-    protected address: string;
-    protected phone: string ;
-    protected events: IEvents 
-    formErrors: FormErrors = {};
+export class UserData {
+	protected payment: string;
+	protected email: string;
+	protected address: string;
+	protected phone: string;
+	protected events: IEvents;
+	formErrors: FormErrors = {};
 
-    constructor(events: IEvents) {
-        this.events = events;
-    }
+	constructor(events: IEvents) {
+		this.events = events;
+	}
 
-    /*setUserOrder(userData: IUser) {
-        this.payment = userData.payment,
-        this.email = userData.email,
-        this.phone = userData.phone,
-        this.payment = userData.payment,
-        this.address = userData.address
-    }*/
+	getUserOrder(): IUser {
+		return {
+			payment: this.payment,
+			email: this.email,
+			address: this.address,
+			phone: this.phone,
+		};
+	}
 
-    getUserOrder(): IUser {
-        return {payment: this.payment, email: this.email, address: this.address, phone: this.phone}
-    }
+	setPaymentForm(field: keyof TUserOrder, value: string) {
+		this[field] = value;
+		this.validationPaymentForm();
+	}
 
-    setPaymentForm(field: keyof TUserOrder, value: string) {
-        this[field] = value;
-        this.validationPaymentForm();
-    }
-
-    validationPaymentForm() {
-        const errors: typeof this.formErrors = {};
+	validationPaymentForm() {
+		const errors: typeof this.formErrors = {};
 
 		if (!this.address) {
 			errors.address = 'Введите адрес';
 		}
 
-        if(!this.payment) {
-            errors.payment = 'Выберите способ оплаты';
-         }
-        
-         this.events.emit('formPaymentErrors:change', errors);
-         return Object.keys(errors).length === 0;
-    }
+		if (!this.payment) {
+			errors.payment = 'Выберите способ оплаты';
+		}
 
-    setContactsForm(field: keyof TUserContacts, value: string) {
-        this[field] = value;
-        this.validationContactsForm();
-    }
+		this.events.emit('formPaymentErrors:change', errors);
+		return Object.keys(errors).length === 0;
+	}
 
-    validationContactsForm() {
-        const errors: typeof this.formErrors = {};
-        if (!this.email) {
-            errors.email = 'Необходимо указать email';
-        }
-        if (!this.phone) {
-            errors.phone = 'Необходимо указать телефон';
-        }
-    
-        this.events.emit('formErrors:change', errors);
-        return Object.keys(errors).length === 0;
-    }
+	setContactsForm(field: keyof TUserContacts, value: string) {
+		this[field] = value;
+		this.validationContactsForm();
+	}
 
-    clearUserData() {
-        this.payment = '',
-        this.email = '',
-        this.address = '',
-        this.phone = ''
-    } 
+	validationContactsForm() {
+		const errors: typeof this.formErrors = {};
+		if (!this.email) {
+			errors.email = 'Необходимо указать email';
+		}
+		if (!this.phone) {
+			errors.phone = 'Необходимо указать телефон';
+		}
 
-    getErrors(): FormErrors {
-        return this.formErrors
-    }
+		this.events.emit('formErrors:change', errors);
+		return Object.keys(errors).length === 0;
+	}
 
-    }
+	clearUserData() {
+		(this.payment = ''),
+			(this.email = ''),
+			(this.address = ''),
+			(this.phone = '');
+	}
 
-
-
-    
-   
-
-
-    
-
-    
-    
-
-
-
+	getErrors(): FormErrors {
+		return this.formErrors;
+	}
+}
